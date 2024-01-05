d0c998d027bbe820d8e700f8e8ee9a83cec3c57b
whokilledravey <ravexion@gmail.com>
2024-01-05 13:36:06 +0700

    update layout script in head

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -26,7 +26,18 @@ export const revalidate = 1;
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive"/>
+      
+      <head>
+        
+
+      <Script 
+      id="Telegram WebApp"
+      type="text/javascript"
+      src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" defer async/>
+      </head>
+
+
+      
         
       
      
