14680241ed8f17d5189436852b0a6e66982f8df0
whokilledravey <ravexion@gmail.com>
2024-01-07 15:35:22 +0700

    fix bb

--- src/app/UI/BackButton/BackButton.jsx
+++ src/app/UI/BackButton/BackButton.jsx
@@ -1,9 +1,10 @@
+'use client'
 import { BackButton } from "@twa-dev/sdk/react";
 
 function Back() {
   // Проверяем, определен ли объект window
   if (typeof window === 'undefined') {
-    return null; // или другая логика, если необходимо
+    return null; 
   }
 
   return (
