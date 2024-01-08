18105303b0f8732b8a6d672e97de29d9f7df6c09
whokilledravey <ravexion@gmail.com>
2024-01-08 22:04:17 +0700

    Add isAuthenticated

--- src/app/UI/getUserId/Validation.jsx
+++ src/app/UI/getUserId/Validation.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 export default function Validation(){
     const [userId, setUserId] = useState("");
-
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
     useEffect(() => {
         const fetchData = async () => {
           try {
