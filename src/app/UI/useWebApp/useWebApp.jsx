6d5d131191f2ae4b8b942f3f93cbaa0e1c15b131
whokilledravey <ravexion@gmail.com>
2024-01-10 13:00:20 +0700

    useWebApp() 3

--- src/app/UI/useWebApp/useWebApp.jsx
+++ src/app/UI/useWebApp/useWebApp.jsx
@@ -1,11 +1,14 @@
 'use client'
-
-
+import { useEffect } from 'react';
 
 export default function useWebApp() {
-  let WebApp = typeof window !== 'undefined' ? window.Telegram.WebApp : null;
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      let WebApp = window.Telegram.WebApp;
       WebApp.expand();
       WebApp.setHeaderColor('secondary_bg_color');
       WebApp.enableClosingConfirmation();
+    }
+  }, []);
 }
 
