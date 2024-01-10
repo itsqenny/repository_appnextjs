6adae60aa513fa9a9362ed14fc90b73d1df1cdff
whokilledravey <ravexion@gmail.com>
2024-01-10 18:39:58 +0700

    .

--- src/app/UI/useWebApp/useWebApp.jsx
+++ src/app/UI/useWebApp/useWebApp.jsx
@@ -4,11 +4,16 @@ export const dynamic = 'force-dynamic';
 
 export default function useWebApp() {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      let WebApp = window.Telegram.WebApp;
-      WebApp?.expand();
-      WebApp?.setHeaderColor('secondary_bg_color');
-      WebApp?.enableClosingConfirmation();
+    // Проверка, что код выполняется на клиентской стороне
+    if (typeof window !== 'undefined' && window.Telegram && window.Telegram.WebApp) {
+      const WebApp = window.Telegram.WebApp;
+      // Выполнение необходимых действий для WebApp
+      WebApp.expand();
+      WebApp.setHeaderColor('secondary_bg_color');
+      WebApp.enableClosingConfirmation();
     }
-  }, []);
+  }, []); // Пустой массив зависимостей гарантирует, что useEffect выполнится только при монтировании компонента
+
+  // Возвращаем null или другое значение, которое вы хотите вернуть
+  return null;
 }
