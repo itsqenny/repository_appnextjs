01202bde7b221c69ceeb11f26ece60a42069fd15
whokilledravey <ravexion@gmail.com>
2024-01-10 12:34:37 +0700

    fix useWebApp

--- src/app/customer/page.jsx
+++ src/app/customer/page.jsx
@@ -1,13 +1,11 @@
 'use client'
 import Link from "next/link";
 import CustomerPhoto from "./customerPhoto";
-import useWebApp from "../UI/useWebApp/useWebApp";
 import Validation from "../UI/getUserId/Validation";
 import { useState } from "react";
 import initData from "../UI/useInitData/initData";
 
 const Header = () => {
-    useWebApp();
     const { user, userId } = initData();
     return (
         <> 
