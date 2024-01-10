d600e7b8bc11ef6935393167d1dd54803782292f
whokilledravey <ravexion@gmail.com>
2024-01-10 21:22:12 +0700

    test 1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,17 +5,20 @@ import CatalogTab from './components/CatalogTab';
 import SearchTab from './components/SearchTab';
 import OpenBanner from './banners/open/banner';
 import BonusTab from './bonus/BonusTab';
-
-
+import { Suspense } from 'react'
+import Loading from './loading';
+L
 export default async function Home(){
     return (
         <>
+        <Suspense fallback={<Loading/>}>
             <Header/>
             <SearchTab/>
             <BonusTab/>
             <OpenBanner/>
             <CatalogTab/>
             <Products/>
+        </Suspense>   
         </>
     );
 };
