2033628665b5520d09ac7cba89c65b6a0bcb2464
whokilledravey <ravexion@gmail.com>
2024-01-10 14:11:25 +0700

    Delete Validation

--- src/app/customer/page.jsx
+++ src/app/customer/page.jsx
@@ -11,7 +11,7 @@ export default function Header(){
   
     return (
         <> 
-         <Validation />
+        
             <nav className="nav-form"> 
                   <Link href={`/customer/${userId}`}>
                    <div className="usercard">
