064c0c898a01f74edf3a0505efec404bd2925273
whokilledravey <ravexion@gmail.com>
2024-01-10 19:08:43 +0700

    add Script in body

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -26,23 +26,13 @@ export const revalidate = 1;
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      
-      <head>
-        
-
-      <Script 
-      id="Telegram WebApp"
-      type="text/javascript"
-      src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" defer/>
-      </head>
-
-
-      
-        
-      
-     
       <body className={inter.className}>
         {children}
+        <Script 
+      id="Telegram WebApp"
+      type="text/javascript"
+      src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" defer async
+      />
       </body>
       
     </html>
