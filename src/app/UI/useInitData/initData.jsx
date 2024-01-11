7862fd00d5888f3a8e3a6ab5236e0881608c03f2
whokilledravey <ravexion@gmail.com>
2024-01-11 11:36:03 +0700

    be back initData

--- src/app/UI/useInitData/initData.jsx
+++ src/app/UI/useInitData/initData.jsx
@@ -1,32 +1,12 @@
 'use client'
 
-import { useEffect, useState } from "react";
-
 
 export default function initData() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const initData = () => {
-      let WebApp = typeof window !== 'undefined' ? window.Telegram.WebApp : null;
-      return {
-        WebApp,
-        user: WebApp?.initDataUnsafe?.user,
-        queryId: WebApp?.initDataUnsafe?.query_id,
-        userId: WebApp?.initDataUnsafe?.user.id,
-      };
-    };
-
-    const result = initData();
-    setData(result);
-
-  }, []); 
-
-  // Здесь оставляем вашу исходную структуру возврата данных
+    let WebApp = typeof window !== 'undefined' ? window.Telegram.WebApp : null;
   return {
-    WebApp: data?.WebApp,
-    user: data?.user,
-    queryId: data?.queryId,
-    userId: data?.userId,
+    WebApp,
+    user: WebApp?.initDataUnsafe?.user,
+    queryId: WebApp?.initDataUnsafe?.query_id,
+    userId: WebApp?.initDataUnsafe?.user.id,
   };
 }
