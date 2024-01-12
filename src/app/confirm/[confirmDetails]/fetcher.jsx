d8e3821e7d3754898374da0e6f7be97d7d305cd3
whokilledravey <ravexion@gmail.com>
2024-01-12 17:06:24 +0700

    update 17:06

--- src/app/confirm/[confirmDetails]/fetcher.jsx
+++ src/app/confirm/[confirmDetails]/fetcher.jsx
@@ -1,15 +1,16 @@
 import useSWR from "swr"
 import axios from 'axios'
  
-const fetcher = url => axios.get(url).then(res => res.data)
+const fetcher = (url, customer) =>
+  axios.post(url, customer).then((res) => res.data);
 
 export default function CustomerStatus({ userId, orderId }) {
     const customer = {
         userId,
         order_id: orderId,
     }
-    const url = 'https://crm.zipperconnect.space/get/payment'
-	const { data, error } = useSWR({url, customer}, fetcher)
+    const url = "https://crm.zipperconnect.space/get/payment";
+	const { data, error } = useSWR([url, customer], fetcher)
 
 	if (error) return "Произошла ошибка."
 	if (!data) return "Загрузка..."
