967bd4ebe2477ad7cda0efe133c39fd2946792db
whokilledravey <ravexion@gmail.com>
2024-01-12 16:31:56 +0700

    update 16:31

--- src/app/confirm/[confirmDetails]/fetcher.jsx
+++ src/app/confirm/[confirmDetails]/fetcher.jsx
@@ -1,23 +1,19 @@
 import useSWR from "swr"
 
-const fetcher = (url, customer) => {
-	return fetch(url, {
-		method: "POST",
-		body: JSON.stringify(customer),
-	}).then((res) => res.json())
-}
+const fetcher = (url) =>
+  fetch(url).then((res) => res.json());
 
 export default function CustomerStatus({ userId, orderId }) {
-	const url = "https://crm.zipperconnect.space/get/payment"
-	const customer = {
-		userId: userId,
-		orderId: orderId,
-	}
+    const url = "https://crm.zipperconnect.space/get/payment";
 
-	const { data, error } = useSWR(() => [url, customer], fetcher)
+    const { data, error } = useSWR(
+      () => (userId && orderId ? [url, { userId, order_id: orderId }] : null),
+      fetcher
+    );
+  
+    if (error) return "Произошла ошибка.";
+    if (!data) return "Загрузка...";
 
-	if (error) return "Произошла ошибка."
-	if (!data) return "Загрузка..."
 	return (
 		<div>
 			<h1>TEST SWR</h1>
