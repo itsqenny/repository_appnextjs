1e19baa21bd3c6472b0753c2810bfe863d39322d
whokilledravey <ravexion@gmail.com>
2024-01-13 02:19:16 +0700

    =update basket 2:18

--- src/app/basket/BasketFetcher.jsx
+++ src/app/basket/BasketFetcher.jsx
@@ -2,28 +2,29 @@ import useSWR from "swr"
 import ProductBasket from "./ProductBasket"
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
-export default function CustomerBasket({userId}) {
+export default function CustomerBasket({ userId }) {
 	const { data, error } = useSWR(
 		`https://crm.zipperconnect.space/customer/basket/${userId}`,
 		fetcher
 	)
 
 	if (error) return "An error has occurred."
-	if (!data) return "Loading..."
-	const { id } = data.basket;
-	const { data: productData, error: productError } = useSWR(
-		`https://repositorydb.onrender.com/products/${id}`,
-		fetcher
-	)
+	if (data) {
+		const { id } = data.basket
+		const { data: productData, error: productError } = useSWR(
+			`https://repositorydb.onrender.com/products/${id}`,
+			fetcher
+		)
 
-	if (productError) return "An error has occurred."
-	if (!productData) return "Loading..."
+		if (productError) return "Произошла ошибка."
+		if (!productData) return "Загрузка..."
 
-
-
-	return (
-		<>
-            <ProductBasket data={data} productData={productData} />
-		</>
-	)
+		return (
+			<>
+				<ProductBasket data={data} productData={productData} />
+			</>
+		)
+	} else {
+		return "Oops..."
+	}
 }
