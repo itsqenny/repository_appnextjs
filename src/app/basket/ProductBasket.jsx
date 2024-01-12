9781c13dfe80ca22cbce44ae55c3eac5035084e9
whokilledravey <ravexion@gmail.com>
2024-01-13 02:26:35 +0700

    UPDATE basket 2:26!

--- src/app/basket/ProductBasket.jsx
+++ src/app/basket/ProductBasket.jsx
@@ -1,71 +1,76 @@
 "use client"
 
-
 import Image from "next/image"
 import { useRouter } from "next/navigation"
 const fetcher = (url) => fetch(url).then((res) => res.json())
 export const revalidate = 0
 const ProductBasket = ({ data }) => {
-  const { data: product, error } = useSWR(
+	const { data: product, error } = useSWR(
 		`https://repositorydb.onrender.com/products/${data.basket.id}`,
 		fetcher
 	)
 
 	if (error) return "An error has occurred."
 	if (!product) return "Loading..."
-  const router = useRouter();
-  const handlePaymentClick = () => {
-    const queryParams = {
-      id: data.basket.id,
-      name: data.basket.name,
-      ConfirmPrice: data.basket.price,
-      ConfirmSize: data.basket.size,
-      orderId: data.basket.order_id,
-    };
-    const queryString = new URLSearchParams(queryParams).toString();
-    router.push(`/confirm/${queryString}`)
-    console.log('send data', queryParams);
-    
-  };
+	const router = useRouter()
+	const handlePaymentClick = () => {
+		const queryParams = {
+			id: data.basket.id,
+			name: data.basket.name,
+			ConfirmPrice: data.basket.price,
+			ConfirmSize: data.basket.size,
+			orderId: data.basket.order_id,
+		}
+		const queryString = new URLSearchParams(queryParams).toString()
+		router.push(`/confirm/${queryString}`)
+		console.log("send data", queryParams)
+	}
 	const basketItems = data.basket.map((item, index) => (
 		<div key={item.order_id} className="product-container-order">
 			<div className="product-swiper">
-					<div className="product-image-component" onClick={handlePaymentClick}>
-						<div className="product-image-container">
-							<div className="product-image-card">
-								<div className="product-image-inner">
-									<div className="product-image-inner-row">
-                  <Image src={product.img[0]} 
-                                 width={200}
-                                 height={160}
-                                 sizes="(max-width: 768px) 100vw,
+				<div className="product-image-component" onClick={handlePaymentClick}>
+					<div className="product-image-container">
+						<div className="product-image-card">
+							<div className="product-image-inner">
+								<div className="product-image-inner-row">
+									<Image
+										src={product.img[0]}
+										width={200}
+										height={160}
+										sizes="(max-width: 768px) 100vw,
                                        (max-width: 1200px) 50vw,
                                        33vw"
-                                 style={{  height:'100%',width: '100%', borderRadius:'17px', WebkitUserSelect:'none', MozUserSelect:'none', userSelect:'none' }}
-                                 
-                                 alt={`${item.name}`}
-                                 priority={true}
-                                 />
-                  </div>
+										style={{
+											height: "100%",
+											width: "100%",
+											borderRadius: "17px",
+											WebkitUserSelect: "none",
+											MozUserSelect: "none",
+											userSelect: "none",
+										}}
+										alt={`${item.name}`}
+										priority={true}
+									/>
 								</div>
 							</div>
 						</div>
 					</div>
+				</div>
 
-					<div className="product-details">
-						<div className="product-price-and-datecreated">
-							<div className="product-price-and-date">
-								<span className="product-price">{item.price}</span>
-								<span className="product-created">Создан: {item.time}</span>
-							</div>
+				<div className="product-details">
+					<div className="product-price-and-datecreated">
+						<div className="product-price-and-date">
+							<span className="product-price">{item.price}</span>
+							<span className="product-created">Создан: {item.time}</span>
 						</div>
-						<div className="product-name">{item.name.toUpperCase()}</div>
-						<span className="product-size">
-							Размер: <b>{item.size}</b>
-						</span>
-
-						<span className="product-size">Оплатить</span>
 					</div>
+					<div className="product-name">{item.name.toUpperCase()}</div>
+					<span className="product-size">
+						Размер: <b>{item.size}</b>
+					</span>
+
+					<span className="product-size">Оплатить</span>
+				</div>
 			</div>
 			<button onClick={() => handleDelete(product.order_id)}>
 				<svg
