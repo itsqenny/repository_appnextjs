8c0eb1cf052d0524b02ec4c3e589ce4d2be2bb9b
whokilledravey <ravexion@gmail.com>
2024-01-12 22:21:37 +0700

    update basket 22:21

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,27 @@
-
-import Products from './products/page';
-import Header from './customer/page';
-import CatalogTab from './components/CatalogTab';
-import SearchTab from './components/SearchTab';
-import OpenBanner from './banners/open/banner';
-import BonusTab from './bonus/BonusTab';
-import { Suspense } from 'react'
-import Loading from './loading';
-import ComingSoon from './banners/comingsoon/banner';
-import BasketItem from './basket/basket';
+import Products from "./products/page"
+import Header from "./customer/page"
+import CatalogTab from "./components/CatalogTab"
+import SearchTab from "./components/SearchTab"
+import OpenBanner from "./banners/open/banner"
+import BonusTab from "./bonus/BonusTab"
+import { Suspense } from "react"
+import Loading from "./loading"
+import ComingSoon from "./banners/comingsoon/banner"
+import BasketItem from "./basket/basket"
 export const revalidate = 0
-export default async function Home(){
-    return (
-        <>
-        <Suspense fallback={<Loading/>}>
-            <Header/>
-            <SearchTab/>
-            <BonusTab/> 
-            <BasketItem/>
-            {/* <OpenBanner/> */}
-            <ComingSoon/>
-            <CatalogTab/>
-            <Products/>
-        </Suspense>   
-        </>
-    );
-};
-
-
+export default async function Home() {
+	return (
+		<>
+			<Suspense fallback={<Loading />}>
+				<Header />
+				<SearchTab />
+				<BonusTab />
+				<BasketItem />
+				{/* <OpenBanner/> */}
+				<ComingSoon />
+				<CatalogTab />
+				<Products />
+			</Suspense>
+		</>
+	)
+}
