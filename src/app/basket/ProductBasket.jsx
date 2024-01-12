218c9e14e446fc5763b746ebd808edf8f357ef94
whokilledravey <ravexion@gmail.com>
2024-01-12 22:08:32 +0700

    update basket 22:08

--- src/app/basket/ProductBasket.jsx
+++ src/app/basket/ProductBasket.jsx
@@ -3,18 +3,18 @@
 
 
 const ProductBasket = ({data}) => {
-    console.log(`basket: ${data.basket}`)
-    const basketItems = data.basket.map(item => (
-        <div key={item.id}>
-            <p>Name: {item.name}</p>
-            <p>Order ID: {item.order_id}</p>
-            <p>Price: {item.price}</p>
-            <p>Size: {item.size}</p>
-            <p>Status: {item.status}</p>
-            <p>Time: {item.time}</p>
-            <hr />
+    
+    const basketItems = data.basket.map((item, index) => (
+        <div key={index}>
+          <p>Name: {item.name}</p>
+          <p>Order ID: {item.order_id}</p>
+          <p>Price: {item.price}</p>
+          <p>Size: {item.size}</p>
+          <p>Status: {item.status}</p>
+          <p>Time: {item.time}</p>
+          <hr />
         </div>
-    ));
+      ));
 
     return (
         <div>
