b14b92a67a0e28e26fea080c9869ced6f0d5713c
whokilledravey <ravexion@gmail.com>
2024-01-13 11:54:39 +0700

    onsol

--- src/app/basket/ProductBasket.jsx
+++ src/app/basket/ProductBasket.jsx
@@ -19,7 +19,6 @@ const ProductBasket = ({ data }) => {
 	//	console.log("send data", queryParams)
 	}
 	
-	
 	const basketItems = data.basket.map((item, index) => (
 		<div key={item.order_id} className="product-container-order">
 			<div className="product-swiper">
