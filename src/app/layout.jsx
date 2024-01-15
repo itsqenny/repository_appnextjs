caf672af46851f7e6ff3b41e18a70e8e8c8d5faa
whokilledravey <ravexion@gmail.com>
2024-01-15 19:38:05 +0700

    update layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,46 +1,49 @@
-import { Inter } from 'next/font/google'
-import '../../styles/customer.css'
-import '../../styles/filters.css'
-import '../../styles/global.css'
-import '../../styles/header.css'
-import '../../styles/item.css'
-import '../../styles/items.css'
-import '../../styles/points.css'
-import '../../styles/search.css'
-import '../../styles/searchTab.css'
-import '../../styles/selectSize.css'
-import '../../styles/stories.css'
-import '../../styles/openBanner.css'
-import '../../styles/ComingSoon.css'
-import '../../styles/basket.css'
-import Head from 'next/head'
-import Script from 'next/script'
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from "next/font/google"
+import "../../styles/customer.css"
+import "../../styles/filters.css"
+import "../../styles/global.css"
+import "../../styles/header.css"
+import "../../styles/item.css"
+import "../../styles/items.css"
+import "../../styles/points.css"
+import "../../styles/search.css"
+import "../../styles/searchTab.css"
+import "../../styles/selectSize.css"
+import "../../styles/stories.css"
+import "../../styles/openBanner.css"
+import "../../styles/ComingSoon.css"
+import "../../styles/basket.css"
+import Head from "next/head"
+import Script from "next/script"
+const inter = Inter({ subsets: ["latin"] })
 
-
-export const revalidate = 1;
+export const revalidate = 1
 
 export const metadata = {
-  title: 'description',
-  description: 'zipperapp',
-  name: 'viewport',
-  content: 'width=device-width',
+	title: "description",
+	description: "zipperapp",
+	name: "viewport",
+	content: "width=device-width",
+	name: "viewport",
+	content:
+		"width=device-width, initial-scale=1.0, interactive-widget=resizes-content, maximum-scale=1",
+	name: "viewport",
+	content: "width=device-width, height=device-height, user-scalable=no",
 }
 export default function RootLayout({ children }) {
-  return (
-    <html lang="ru" suppressHydrationWarning> 
-      
-      <body className={inter.className} suppressHydrationWarning={true}>
-        {children}
-        <Script 
-        id="Telegram WebApp"
-        type="text/javascript"
-        src="https://telegram.org/js/telegram-web-app.js" 
-        strategy="beforeInteractive" 
-        defer 
-        async/>
-      </body>
-      
-    </html>
-  )
+	return (
+		<html lang="ru" suppressHydrationWarning>
+			<body className={inter.className} suppressHydrationWarning={true}>
+				{children}
+				<Script
+					id="Telegram WebApp"
+					type="text/javascript"
+					src="https://telegram.org/js/telegram-web-app.js"
+					strategy="beforeInteractive"
+					defer
+					async
+				/>
+			</body>
+		</html>
+	)
 }
