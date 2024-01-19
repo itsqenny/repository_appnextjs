4e9fac9958f408db28e69e901a8851bd2bf3ebfd
whokilledravey <ravexion@gmail.com>
2024-01-19 22:10:38 +0700

    update 22:10

--- src/app/UI/BackButton/BackButton.jsx
+++ src/app/UI/BackButton/BackButton.jsx
@@ -4,7 +4,11 @@ import { BackButton } from "@twa-dev/sdk/react"
 export const dynamic = 'force-dynamic'
 function Back(){
     const handleBackClick = () => {
+        // Отключаем прокрутку вверх при нажатии на кнопку Back
         window.scrollTo(0, 0);
+
+        // Возвращаемся назад в истории
+        window.history.back();
     };
     return (
     <>
