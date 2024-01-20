29e39e8d4c10d9f9a0c85656b61d13e8846b5891
whokilledravey <ravexion@gmail.com>
2024-01-20 11:59:33 +0700

    fix bb 11:59

--- src/app/UI/BackButton/BackButton.jsx
+++ src/app/UI/BackButton/BackButton.jsx
@@ -4,14 +4,11 @@ import { BackButton } from "@twa-dev/sdk/react"
 import { useRouter } from 'next/navigation'
 export const dynamic = 'force-dynamic'
 function Back(){
-    const router = useRouter()
-    const handleBackClick = () => {
-        router.push('/', { scroll: true});
-    };
+   
     return (
     <>
 
-    <BackButton onClick={handleBackClick} />
+    <BackButton />
     </>
     );
 };
