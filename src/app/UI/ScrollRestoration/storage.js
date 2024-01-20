677327792725d511629b5ab5c302b79f4f37d96a
whokilledravey <ravexion@gmail.com>
2024-01-20 15:58:30 +0700

    fix replaceState 15:58

--- src/app/UI/ScrollRestoration/storage.js
+++ src/app/UI/ScrollRestoration/storage.js
@@ -1,30 +1,44 @@
 'use client'
+
 export const ScrollPos = [0, 0];
 const uniq = 'revotale_nextjs_scroll_restoration';
 const getKey = (pos) => `${uniq}_${pos}`;
 export const HistoryState = null;
 
+let replaceStateCounter = 0;
+const replaceStateLimit = 70;
+const replaceStateInterval = 10000; // 10 секунд
+
 export const setCurrentScrollHistory = ([x, y]) => {
     console.log(`Remember history scroll to ${x} ${y}. Href ${window.location.href}.`);
-    
-    // Проверяем, являются ли x и y числами
-    if (isNaN(x) || isNaN(y)) {
-        console.error('Error: x and y must be numbers.');
-        return;
-    }
-
     x = Math.max(x, 0);
     y = Math.max(y, 0);
 
     const newState = (window.history.state) ?? {};
-    window.history.replaceState(
-        {
-            ...newState,
-            [getKey('x')]: x,
-            [getKey('y')]: y,
-        },
-        ''
-    );
+    const updatedState = {
+        ...newState,
+        [getKey('x')]: x,
+        [getKey('y')]: y,
+    };
+
+    // Увеличиваем счетчик
+    replaceStateCounter++;
+
+    // Выводим информацию в консоль перед вызовом replaceState
+    console.log(`Calling replaceState. Counter: ${replaceStateCounter}, Limit: ${replaceStateLimit}`);
+
+    // Вызываем replaceState только если не превышено ограничение и прошло достаточно времени
+    if (replaceStateCounter <= replaceStateLimit) {
+        window.history.replaceState(updatedState, '');
+    }
+
+    // Выводим информацию в консоль после вызова replaceState
+    console.log(`replaceState called. Counter: ${replaceStateCounter}, Limit: ${replaceStateLimit}`);
+
+    // Сбрасываем счетчик после интервала
+    setTimeout(() => {
+        replaceStateCounter = 0;
+    }, replaceStateInterval);
 };
 
 export const getScrollFromState = (state) => {
