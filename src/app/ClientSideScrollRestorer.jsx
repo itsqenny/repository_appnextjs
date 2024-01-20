98a3f7be2d38d81c3b84091ac6d17644f5c16061
whokilledravey <ravexion@gmail.com>
2024-01-20 12:19:38 +0700

    test 12:19

--- src/app/ClientSideScrollRestorer.jsx
+++ src/app/ClientSideScrollRestorer.jsx
@@ -1,8 +1,11 @@
 "use client"
 import { useScrollRestorer } from 'next-scroll-restorer';
-
+import eruda from 'eruda'
 const ClientSideScrollRestorer = () => {
+    
   if (typeof window !== 'undefined') {
+
+      eruda.init();
     useScrollRestorer();
   }
 
