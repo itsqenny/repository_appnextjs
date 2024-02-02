f9be296091c3d0c860c30fc0b02c5ff52624a33e
whokilledravey <ravexion@gmail.com>
2024-02-02 21:48:26 +0700

    update api

--- src/app/api/products/[productId]/route.jsx
+++ src/app/api/products/[productId]/route.jsx
@@ -1,12 +1,10 @@
 export async function GET(request) {
-	const api = process.env.NEXT_PUBLIC_API_PRODUCT_LIST;
+	const api = process.env.NEXT_PUBLIC_API_PRODUCT_LIST
 	try {
 		const url = new URL(request.url || request)
 		const pathnameParts = url.pathname.split("/").filter((part) => part !== "")
 		const productId = pathnameParts[pathnameParts.length - 1]
-		const res = await fetch(
-			`${api}/products/${productId}`
-		)
+		const res = await fetch(`${api}/api/products/${productId}`)
 		const item = await res.json()
 
 		return new Response(JSON.stringify(item))
--- src/app/api/products/route.jsx
+++ src/app/api/products/route.jsx
@@ -1,12 +1,11 @@
+export const revalidate = 10
 
-export const revalidate = 10;
+export async function GET(req) {
+	const api = process.env.API_PRODUCTS
+	const { searchParams } = new URL(req.url)
+	const page = searchParams.get("_page")
+	const res = await fetch(`${api}/api/product?limit=10&page=${page}`)
+	const items = await res.json()
 
-export async function GET(req){
-    const api = process.env.API_PRODUCTS;
-    const { searchParams } = new URL(req.url)
-    const page = searchParams.get('_page')
-    const res = await fetch(`${api}/products?_page=${page}`)
-    const items = await res.json()
-    
-    return new Response(JSON.stringify(items))
+	return new Response(JSON.stringify(items))
 }
