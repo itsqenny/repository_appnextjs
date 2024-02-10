cc91dc10428ac93bc9c5045c8ed3b036433f34c6
whokilledravey <ravexion@gmail.com>
2024-02-10 20:44:00 +0700

    123

--- src/app/customer/Customer.jsx
+++ src/app/customer/Customer.jsx
@@ -1,22 +1,17 @@
-'use client'
-import Validation from "../UI/getUserId/Validation";
-import initData from "../UI/useInitData/initData";
-import useWebApp from "../UI/useWebApp/useWebApp";
-import PhotoFetcher from "./PhotoFetcher";
+"use client"
+import Validation from "../UI/getUserId/Validation"
+import initData from "../UI/useInitData/initData"
+import useWebApp from "../UI/useWebApp/useWebApp"
+import PhotoFetcher from "./PhotoFetcher"
 
-
-export default function Customer(){
-    const { user, userId } = initData();
-    useWebApp();
-    //const userId = '204688184'
-    //const user = { username: 'whokilledravey', first_name: 'Евгений'}
-    return (
-        <> 
-        <Validation/>
-           
-            <PhotoFetcher userId={userId} user={user}/>
-        </>
-    );
-};
-
-
+export default function Customer() {
+	//const { user, userId } = initData();
+	useWebApp()
+	//const userId = "204688184"
+	//const user = { username: "whokilledravey", first_name: "Евгений" }
+	return (
+		<>
+			<PhotoFetcher userId={userId} user={user} />
+		</>
+	)
+}
