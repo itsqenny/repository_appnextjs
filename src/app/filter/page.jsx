c987ab8127777e58b334a7c7a88dd61a2a85f9cb
ravey <ravexion@gmail.com>
2024-02-22 09:48:47 +0700

    fix back

--- src/app/filter/page.jsx
+++ src/app/filter/page.jsx
@@ -1,12 +1,13 @@
 "use client"
 import { BackButton } from "@twa-dev/sdk/react"
 import Filter from "./Filter"
-
+import Dynamic from "next/dynamic"
+const Back = Dynamic(() => import("../UI/BackButton/BackButton"))
 export default function FilterPage() {
 	if (typeof window !== "undefined") {
 		return (
 			<>
-				<BackButton />
+				<Back />
 				<Filter />
 			</>
 		)
