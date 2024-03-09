16ac87e7ca1e5eaa844e062dfc1da482d7b8b084
ravey <ravexion@gmail.com>
2024-03-10 01:20:04 +0700

    added initdata

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,5 @@
+"use client"
+import { useMemo } from "react"
 import ProductsPage from "./products/page"
 import CatalogTab from "./components/CatalogTab"
 import SearchTab from "./components/SearchTab"
@@ -8,10 +10,53 @@ import BasketItem from "./basket/basket"
 import PaidBasket from "./paidbasket/paidBasket"
 import Customer from "./customer/Customer"
 import Stories from "./stories/page"
+import { useInitData } from "@tma.js/sdk-react"
+
+function InitData() {
+	const initData = useInitData()
+
+	const initDataJson = useMemo(() => {
+		if (!initData) {
+			return "Init data is empty."
+		}
+		const {
+			authDate,
+			chat,
+			hash,
+			canSendAfter,
+			queryId,
+			receiver,
+			user,
+			startParam,
+		} = initData
+
+		return JSON.stringify(
+			{
+				authDate,
+				chat,
+				hash,
+				canSendAfter,
+				queryId,
+				receiver,
+				user,
+				startParam,
+			},
+			null,
+			" "
+		)
+	}, [initData])
+
+	return (
+		<pre>
+			<code>{initDataJson}</code>
+		</pre>
+	)
+}
 
 export default async function Home() {
 	return (
 		<>
+			<InitData />
 			<Customer />
 			<SearchTab />
 			<Stories />
